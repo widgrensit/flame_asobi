9e388858afb43fbe4edeaeff24147b890961101f
Daniel Widgren <daniel.widgren@gmail.com>
2026-04-01 23:27:45 +0200

    feat: add voting support to Flame bridge

--- lib/flame_asobi.dart
+++ lib/flame_asobi.dart
@@ -1,7 +1,8 @@
 export 'package:asobi/asobi.dart';
-export 'src/has_asobi.dart';
+export 'src/asobi_input_sender.dart';
+export 'src/asobi_matchmaker.dart';
 export 'src/asobi_network_sync.dart';
 export 'src/asobi_player.dart';
+export 'src/asobi_vote_listener.dart';
 export 'src/asobi_projectile.dart';
-export 'src/asobi_matchmaker.dart';
-export 'src/asobi_input_sender.dart';
+export 'src/has_asobi.dart';
